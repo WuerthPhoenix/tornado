4d4a7bfa71dea15437df55ffaedc094f060925bc
Francesco Cina <mr.francesco.cina@gmail.com>
2018-10-25 13:21:46 +0200

    Add dispatcher stub

--- src/engine/matcher/src/dispatcher/mod.rs
+++ src/engine/matcher/src/dispatcher/mod.rs
@@ -1,1 +1,24 @@
-mod action;
+pub mod action;
+
+#[derive(Default)]
+pub struct Dispatcher {
+    actions: Vec<action::DispatcherAction>
+}
+
+impl Dispatcher {
+    pub fn new(rules: &[Rule]) -> Result<Dispatcher, MatcherError> {
+
+        let action_builder = action::DispatcherActionBuilder::new();
+
+        let mut dispatcher = Dispatcher{
+            actions: vec![]
+        };
+
+        for rule in rules {
+            //action_builder.build(rule.)
+        }
+
+        Ok(dispatcher)
+    }
+}
+
