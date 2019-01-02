6c11945f635784a59b80f183633ee4f1438bab71
Francesco Cina <mr.francesco.cina@gmail.com>
2019-01-02 17:58:44 +0100

    Fix TOR-36 - add best and worst case benches

--- src/engine/matcher/benches/matcher_bench.rs
+++ src/engine/matcher/benches/matcher_bench.rs
@@ -4,6 +4,6 @@ pub mod utils;
 use crate::matcher::*;
 use criterion::*;
 
-criterion_group!(benches, one_simple_rule::bench, trap::bench);
+criterion_group!(benches, one_simple_rule::bench, no_match::bench, full_match::bench);
 
 criterion_main!(benches);
