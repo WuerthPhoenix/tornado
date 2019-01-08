9d19674b35d08b498db14d17d50f75a0ccaa0b4f
Michele Santuari <michele.santuari@wuerth-phoenix.com>
2019-01-08 12:30:23 +0100

    test for inital parsing of github webhook

--- src/spike/webhook/src/main.rs
+++ src/spike/webhook/src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::http::Method;
 use actix_web::{server, App, HttpRequest, Json, Responder, Result};
 use serde_json::Value;
+use tornado_common_api::Event;
 
 fn greet(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
@@ -8,8 +9,18 @@ fn greet(req: &HttpRequest) -> impl Responder {
     format!("Hello {}!\n", to)
 }
 
-fn post_event(evt: Json<Value>) -> Result<String> {
-    println!("received event: \n{:#?}", evt);
+fn post_event(evt: Json<jmespath::Variable>) -> Result<String> {
+    let mut event = Event::new("webhook");
+    // Act
+    let expr = jmespath::compile("commits[0].author.name").unwrap();
+//    let data = jmespath::Variable::from_json(&event_json).unwrap();
+
+    let result = expr.search(evt.as_object()).unwrap();
+
+    // Assert
+
+    println!("!!!! {}\n ", result.as_string().unwrap());
+//    assert_eq!("email", result.as_string().unwrap());
     Ok(format!("got event \n{:#?}\n", evt))
 }
 
@@ -36,6 +47,7 @@ mod test {
     use actix_web::{HttpRequest, HttpMessage, http};
     use actix_web::test::TestServer;
     use tornado_common_api::Event;
+    use std::fs;
 
     #[test]
     fn should_get_hello_world() {
@@ -56,9 +68,10 @@ mod test {
     fn should_post_an_event() {
         // start new test server
         let mut srv = TestServer::with_factory(create_app);
-
+        let filename = "./test_resources/github-push.json";
+        let github_json = fs::read_to_string(filename).expect(&format!("Unable to open the file [{}]", filename));
         let request = srv.client(
-            http::Method::POST, "/event").json(Event::new("Prova1")).unwrap();
+            http::Method::POST, "/event").content_type("application/json").body(github_json).unwrap();
         let response = srv.execute(request.send()).unwrap();
         assert!(response.status().is_success());
 
@@ -68,4 +81,16 @@ mod test {
         assert_eq!(body, "Hello world!");
         */
     }
+
+//    GitHub Headers:
+//    Request URL: http://35.240.75.114/event
+//    Request method: POST
+//    content-type: application/json
+//    Expect:
+//    User-Agent: GitHub-Hookshot/60c6631
+//    X-GitHub-Delivery: 78aca354-1335-11e9-8f50-3fb6173890ad
+//    X-GitHub-Event: push
+
+
+
 }
