d26b7009f3f77bd2d1e7fac04149409b66302d41
Francesco Cina <mr.francesco.cina@gmail.com>
2019-01-08 11:01:21 +0100

    [spike-webhook] Add tests

--- src/spike/webhook/src/main.rs
+++ src/spike/webhook/src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::http::Method;
 use actix_web::{server, App, HttpRequest, Json, Responder, Result};
-use tornado_common_api::Event;
+use serde_json::Value;
 
 fn greet(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
@@ -8,19 +8,63 @@ fn greet(req: &HttpRequest) -> impl Responder {
     format!("Hello {}!\n", to)
 }
 
-fn get_event(evt: Json<Event>) -> Result<String> {
+fn get_event(evt: Json<Value>) -> Result<String> {
     println!("received event: \n{:#?}", evt);
     Ok(format!("got event \n{:#?}\n", evt))
 }
 
 fn main() {
     server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
-            .resource("/event", |r| r.method(Method::POST).with(get_event))
+        create_app()
     })
     .bind("0.0.0.0:80")
     .expect("Can not bind to port 80")
     .run();
+}
+
+fn create_app() -> App {
+    App::new()
+        .resource("/", |r| r.method(Method::GET).f(greet))
+        .resource("/{name}", |r| r.method(Method::GET).f(greet))
+        .resource("/event", |r| r.method(Method::POST).with(get_event))
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use actix_web::{HttpRequest, HttpMessage, http};
+    use actix_web::test::TestServer;
+
+    #[test]
+    fn should_get_hello_world() {
+        // start new test server
+        let mut srv = TestServer::with_factory(create_app);
+
+        let request = srv.client(
+            http::Method::GET, "/").finish().unwrap();
+        let response = srv.execute(request.send()).unwrap();
+        assert!(response.status().is_success());
+
+        let bytes = srv.execute(response.body()).unwrap();
+        let body = std::str::from_utf8(&bytes).unwrap();
+        assert_eq!(body, "Hello World!\n");
+    }
+
+    #[test]
+    fn should_post_an_event() {
+        // start new test server
+        let mut srv = TestServer::with_factory(create_app);
+
+        let request = srv.client(
+            http::Method::POST, "/event").finish().unwrap();
+        let response = srv.execute(request.send()).unwrap();
+        assert!(response.status().is_success());
+
+        /*
+        let bytes = srv.execute(response.body()).unwrap();
+        let body = str::from_utf8(&bytes).unwrap();
+        assert_eq!(body, "Hello world!");
+        */
+    }
 }
