f2c8a3ccf0f161ef7d7b81747b0a771f87c03128
Francesco Cina <mr.francesco.cina@gmail.com>
2019-01-10 08:48:16 +0100

    [collector-jmespath] Add EventProcessor

--- src/collector/jmespath/src/lib.rs
+++ src/collector/jmespath/src/lib.rs
@@ -20,6 +20,31 @@ impl<'a> Collector<&'a str> for JMESPathEventCollector {
     }
 }
 
+
+struct EventProcessor {
+    event_type: ValueProcessor
+}
+
+const EXPRESSION_START_DELIMITER: &str = "${";
+const EXPRESSION_END_DELIMITER: &str = "}";
+
+impl EventProcessor {
+
+    pub fn build(config: &config::JMESPathEventCollectorConfig) -> Result<EventProcessor, CollectorError> {
+        Ok(EventProcessor{event_type: EventProcessor::build_value(&config.event_type)?})
+    }
+
+    fn build_value(value: &str)  -> Result<ValueProcessor, CollectorError> {
+        if value.starts_with( EXPRESSION_START_DELIMITER) && value.ends_with( EXPRESSION_END_DELIMITER) {
+            let expression= & value[ EXPRESSION_START_DELIMITER.len()..(value.len() - EXPRESSION_END_DELIMITER.len())];
+            Ok(ValueProcessor::Expression{exp: jmespath::compile(expression).unwrap()})
+        } else {
+            Ok(ValueProcessor::Text {text: value.to_owned()})
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 enum ValueProcessor {
     Expression { exp: jmespath::Expression<'static> },
     Text { text: String },
@@ -27,7 +52,7 @@ enum ValueProcessor {
 
 impl ValueProcessor {
 
-    fn value<F>(&self, var: &jmespath::Variable, func: F) -> Result<(), CollectorError>
+    pub fn get_value<F>(&self, var: &jmespath::Variable, func: F) -> Result<(), CollectorError>
     where
         F: FnOnce(&str) -> Result<(), CollectorError>,
     {
@@ -59,34 +84,149 @@ mod test {
 
     use super::*;
     use std::fs;
+    use std::sync::Mutex;
+    use std::rc::Rc;
 
     #[test]
-    fn should_return_an_event() {
+    fn value_processor_text_should_return_static_text() {
         // Arrange
-        let event = Event::new(String::from("email"));
-        let json = serde_json::to_string(&event).unwrap();
-
-        let collector = JMESPathEventCollector::new();
+        let value_proc = ValueProcessor::Text {text: "hello world".to_owned()};
+        let json = r#"
+        {
+            "level_one": {
+                "level_two": "level_two_value"
+            }
+        }
+        "#;
+        let data = jmespath::Variable::from_json(&json).unwrap();
+        let atomic = Rc::new(Mutex::new("".to_owned()));
+        let atomic_clone = atomic.clone();
 
         // Act
-        let from_json = collector.to_event(&json).unwrap();
+        let result = value_proc.get_value(&data, move |value| {
+            let mut lock = atomic_clone.lock().unwrap();
+            *lock = value.to_owned();
+            Ok(())
+        });
 
         // Assert
-        assert_eq!(event.event_type, from_json.event_type);
-        assert_eq!(event.created_ts, from_json.created_ts);
+        assert!(result.is_ok());
+        assert_eq!("hello world", *atomic.lock().unwrap());
+
     }
 
     #[test]
-    fn should_return_an_error() {
+    fn value_processor_expression_should_return_from_json() {
         // Arrange
-        let json = "{message: 'hello_world'}".to_owned();
-        let collector = JMESPathEventCollector::new();
+        let exp = jmespath::compile("level_one.level_two").unwrap();
+        let value_proc = ValueProcessor::Expression {exp};
+        let json = r#"
+        {
+            "level_one": {
+                "level_two": "level_two_value"
+            }
+        }
+        "#;
+        let data = jmespath::Variable::from_json(&json).unwrap();
+        let atomic = Rc::new(Mutex::new("".to_owned()));
+        let atomic_clone = atomic.clone();
 
         // Act
-        let result = collector.to_event(&json);
+        let result = value_proc.get_value(&data, move |value| {
+            let mut lock = atomic_clone.lock().unwrap();
+            *lock = value.to_owned();
+            Ok(())
+        });
 
         // Assert
-        assert!(result.is_err())
+        assert!(result.is_ok());
+        assert_eq!("level_two_value", *atomic.lock().unwrap());
+
+    }
+
+    #[test]
+    fn value_processor_expression_should_return_error_if_not_present() {
+        // Arrange
+        let exp = jmespath::compile("level_one.level_three").unwrap();
+        let value_proc = ValueProcessor::Expression {exp};
+        let json = r#"
+        {
+            "level_one": {
+                "level_two": "level_two_value"
+            }
+        }
+        "#;
+        let data = jmespath::Variable::from_json(&json).unwrap();
+        let atomic = Rc::new(Mutex::new("".to_owned()));
+        let atomic_clone = atomic.clone();
+
+        // Act
+        let result = value_proc.get_value(&data, move |value| {
+            let mut lock = atomic_clone.lock().unwrap();
+            *lock = value.to_owned();
+            Ok(())
+        });
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!("", *atomic.lock().unwrap());
+
+    }
+
+    #[test]
+    fn value_processor_expression_should_return_error_if_not_present_in_array() {
+        // Arrange
+        let exp = jmespath::compile("level_one.level_two[2]").unwrap();
+        let value_proc = ValueProcessor::Expression {exp};
+        let json = r#"
+        {
+            "level_one": {
+                "level_two": ["level_two_0", "level_two_1"]
+            }
+        }
+        "#;
+        let data = jmespath::Variable::from_json(&json).unwrap();
+        let atomic = Rc::new(Mutex::new("".to_owned()));
+        let atomic_clone = atomic.clone();
+
+        // Act
+        let result = value_proc.get_value(&data, move |value| {
+            let mut lock = atomic_clone.lock().unwrap();
+            *lock = value.to_owned();
+            Ok(())
+        });
+
+        // Assert
+        assert!(result.is_err());
+        assert_eq!("", *atomic.lock().unwrap());
+
+    }
+
+    #[test]
+    fn event_processor_should_build_from_config_with_static_type() {
+        // Arrange
+        let config = config::JMESPathEventCollectorConfig{event_type: "hello world".to_owned()};
+
+        // Act
+        let event_processor = EventProcessor::build(&config).unwrap();
+
+        // Assert
+        assert_eq!(ValueProcessor::Text {text: "hello world".to_owned()}, event_processor.event_type);
+
+    }
+
+    #[test]
+    fn event_processor_should_build_from_config_with_expression() {
+        // Arrange
+        let config = config::JMESPathEventCollectorConfig{event_type: "${first.second[0]}".to_owned()};
+        let expected_expression = jmespath::compile("first.second[0]").unwrap();
+
+        // Act
+        let event_processor = EventProcessor::build(&config).unwrap();
+
+        // Assert
+        assert_eq!(ValueProcessor::Expression {exp: expected_expression}, event_processor.event_type);
+
     }
 
 }
