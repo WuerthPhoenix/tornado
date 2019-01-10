3b48cf883e8ba26060fbfd323d793ae48ee395e3
Francesco Cina <mr.francesco.cina@gmail.com>
2019-01-10 10:08:43 +0100

    Add full tests

--- src/collector/jmespath/src/lib.rs
+++ src/collector/jmespath/src/lib.rs
@@ -4,43 +4,63 @@ use tornado_common_api::{Event, Payload};
 pub mod config;
 
 ///A collector that receives an input in JSON format and allows the creation of Events using the JMESPath JSON query language.
-#[derive(Default)]
-pub struct JMESPathEventCollector {}
+pub struct JMESPathEventCollector {
+    processor: EventProcessor,
+}
 
 impl JMESPathEventCollector {
-    pub fn new() -> JMESPathEventCollector {
-        Default::default()
+    pub fn new(
+        config: &config::JMESPathEventCollectorConfig,
+    ) -> Result<JMESPathEventCollector, CollectorError> {
+        let processor = EventProcessor::build(config)?;
+        Ok(JMESPathEventCollector { processor })
     }
 }
 
 impl<'a> Collector<&'a str> for JMESPathEventCollector {
     fn to_event(&self, input: &'a str) -> Result<Event, CollectorError> {
-        serde_json::from_str::<tornado_common_api::Event>(&input)
-            .map_err(|e| CollectorError::EventCreationError { message: format!("{}", e) })
+        let data = jmespath::Variable::from_json(input).map_err(|err| {
+            CollectorError::EventCreationError {
+                message: format!("Cannot parse received json. Err: {} - Json: {}.", err, input),
+            }
+        })?;
+        self.processor.process(&data)
     }
 }
 
-
 struct EventProcessor {
-    event_type: ValueProcessor
+    event_type: ValueProcessor,
 }
 
 const EXPRESSION_START_DELIMITER: &str = "${";
 const EXPRESSION_END_DELIMITER: &str = "}";
 
 impl EventProcessor {
-
-    pub fn build(config: &config::JMESPathEventCollectorConfig) -> Result<EventProcessor, CollectorError> {
-        Ok(EventProcessor{event_type: EventProcessor::build_value(&config.event_type)?})
+    pub fn build(
+        config: &config::JMESPathEventCollectorConfig,
+    ) -> Result<EventProcessor, CollectorError> {
+        Ok(EventProcessor { event_type: EventProcessor::build_value(&config.event_type)? })
     }
 
-    fn build_value(value: &str)  -> Result<ValueProcessor, CollectorError> {
-        if value.starts_with( EXPRESSION_START_DELIMITER) && value.ends_with( EXPRESSION_END_DELIMITER) {
-            let expression= & value[ EXPRESSION_START_DELIMITER.len()..(value.len() - EXPRESSION_END_DELIMITER.len())];
-            Ok(ValueProcessor::Expression{exp: jmespath::compile(expression).unwrap()})
+    fn build_value(value: &str) -> Result<ValueProcessor, CollectorError> {
+        if value.starts_with(EXPRESSION_START_DELIMITER)
+            && value.ends_with(EXPRESSION_END_DELIMITER)
+        {
+            let expression = &value
+                [EXPRESSION_START_DELIMITER.len()..(value.len() - EXPRESSION_END_DELIMITER.len())];
+            Ok(ValueProcessor::Expression { exp: jmespath::compile(expression).unwrap() })
         } else {
-            Ok(ValueProcessor::Text {text: value.to_owned()})
+            Ok(ValueProcessor::Text { text: value.to_owned() })
         }
+    }
+
+    pub fn process(&self, var: &jmespath::Variable) -> Result<Event, CollectorError> {
+        let mut event = Event::new("");
+        self.event_type.process(var, |value| {
+            event.event_type = value.to_owned();
+            Ok(())
+        })?;
+        Ok(event)
     }
 }
 
@@ -51,8 +71,7 @@ enum ValueProcessor {
 }
 
 impl ValueProcessor {
-
-    pub fn get_value<F>(&self, var: &jmespath::Variable, func: F) -> Result<(), CollectorError>
+    pub fn process<F>(&self, var: &jmespath::Variable, func: F) -> Result<(), CollectorError>
     where
         F: FnOnce(&str) -> Result<(), CollectorError>,
     {
@@ -76,7 +95,6 @@ impl ValueProcessor {
             ValueProcessor::Text { text } => func(&text),
         }
     }
-
 }
 
 #[cfg(test)]
@@ -84,13 +102,13 @@ mod test {
 
     use super::*;
     use std::fs;
-    use std::sync::Mutex;
     use std::rc::Rc;
+    use std::sync::Mutex;
 
     #[test]
     fn value_processor_text_should_return_static_text() {
         // Arrange
-        let value_proc = ValueProcessor::Text {text: "hello world".to_owned()};
+        let value_proc = ValueProcessor::Text { text: "hello world".to_owned() };
         let json = r#"
         {
             "level_one": {
@@ -103,7 +121,7 @@ mod test {
         let atomic_clone = atomic.clone();
 
         // Act
-        let result = value_proc.get_value(&data, move |value| {
+        let result = value_proc.process(&data, move |value| {
             let mut lock = atomic_clone.lock().unwrap();
             *lock = value.to_owned();
             Ok(())
@@ -112,14 +130,13 @@ mod test {
         // Assert
         assert!(result.is_ok());
         assert_eq!("hello world", *atomic.lock().unwrap());
-
     }
 
     #[test]
     fn value_processor_expression_should_return_from_json() {
         // Arrange
         let exp = jmespath::compile("level_one.level_two").unwrap();
-        let value_proc = ValueProcessor::Expression {exp};
+        let value_proc = ValueProcessor::Expression { exp };
         let json = r#"
         {
             "level_one": {
@@ -132,7 +149,7 @@ mod test {
         let atomic_clone = atomic.clone();
 
         // Act
-        let result = value_proc.get_value(&data, move |value| {
+        let result = value_proc.process(&data, move |value| {
             let mut lock = atomic_clone.lock().unwrap();
             *lock = value.to_owned();
             Ok(())
@@ -141,14 +158,13 @@ mod test {
         // Assert
         assert!(result.is_ok());
         assert_eq!("level_two_value", *atomic.lock().unwrap());
-
     }
 
     #[test]
     fn value_processor_expression_should_return_error_if_not_present() {
         // Arrange
         let exp = jmespath::compile("level_one.level_three").unwrap();
-        let value_proc = ValueProcessor::Expression {exp};
+        let value_proc = ValueProcessor::Expression { exp };
         let json = r#"
         {
             "level_one": {
@@ -161,7 +177,7 @@ mod test {
         let atomic_clone = atomic.clone();
 
         // Act
-        let result = value_proc.get_value(&data, move |value| {
+        let result = value_proc.process(&data, move |value| {
             let mut lock = atomic_clone.lock().unwrap();
             *lock = value.to_owned();
             Ok(())
@@ -170,14 +186,13 @@ mod test {
         // Assert
         assert!(result.is_err());
         assert_eq!("", *atomic.lock().unwrap());
-
     }
 
     #[test]
     fn value_processor_expression_should_return_error_if_not_present_in_array() {
         // Arrange
         let exp = jmespath::compile("level_one.level_two[2]").unwrap();
-        let value_proc = ValueProcessor::Expression {exp};
+        let value_proc = ValueProcessor::Expression { exp };
         let json = r#"
         {
             "level_one": {
@@ -190,7 +205,7 @@ mod test {
         let atomic_clone = atomic.clone();
 
         // Act
-        let result = value_proc.get_value(&data, move |value| {
+        let result = value_proc.process(&data, move |value| {
             let mut lock = atomic_clone.lock().unwrap();
             *lock = value.to_owned();
             Ok(())
@@ -199,34 +214,88 @@ mod test {
         // Assert
         assert!(result.is_err());
         assert_eq!("", *atomic.lock().unwrap());
-
     }
 
     #[test]
     fn event_processor_should_build_from_config_with_static_type() {
         // Arrange
-        let config = config::JMESPathEventCollectorConfig{event_type: "hello world".to_owned()};
+        let config = config::JMESPathEventCollectorConfig { event_type: "hello world".to_owned() };
 
         // Act
         let event_processor = EventProcessor::build(&config).unwrap();
 
         // Assert
-        assert_eq!(ValueProcessor::Text {text: "hello world".to_owned()}, event_processor.event_type);
-
+        assert_eq!(
+            ValueProcessor::Text { text: "hello world".to_owned() },
+            event_processor.event_type
+        );
     }
 
     #[test]
     fn event_processor_should_build_from_config_with_expression() {
         // Arrange
-        let config = config::JMESPathEventCollectorConfig{event_type: "${first.second[0]}".to_owned()};
+        let config =
+            config::JMESPathEventCollectorConfig { event_type: "${first.second[0]}".to_owned() };
         let expected_expression = jmespath::compile("first.second[0]").unwrap();
 
         // Act
         let event_processor = EventProcessor::build(&config).unwrap();
 
         // Assert
-        assert_eq!(ValueProcessor::Expression {exp: expected_expression}, event_processor.event_type);
+        assert_eq!(
+            ValueProcessor::Expression { exp: expected_expression },
+            event_processor.event_type
+        );
+    }
 
+    #[test]
+    fn verify_expected_io() {
+        verify_io(
+            "./test_resources/01_config.json",
+            "./test_resources/01_input.json",
+            "./test_resources/01_output.json",
+        );
+        verify_io(
+            "./test_resources/02_config.json",
+            "./test_resources/02_input.json",
+            "./test_resources/02_output.json",
+        );
+        verify_io(
+            "./test_resources/github_webhook_01_config.json",
+            "./test_resources/github_webhook_01_input.json",
+            "./test_resources/github_webhook_01_output.json",
+        );
+    }
+
+    fn verify_io(config_path: &str, input_path: &str, output_path: &str) {
+        // Arrange
+        let config_json = fs::read_to_string(config_path)
+            .expect(&format!("Unable to open the file [{}]", config_path));
+        let config: config::JMESPathEventCollectorConfig = serde_json::from_str(&config_json)
+            .map_err(|e| panic!("Cannot parse config json. Err: {}", e))
+            .unwrap();
+
+        let collector = JMESPathEventCollector::new(&config).unwrap();
+
+        let input_json = fs::read_to_string(input_path)
+            .expect(&format!("Unable to open the file [{}]", input_path));
+
+        let output_json = fs::read_to_string(output_path)
+            .expect(&format!("Unable to open the file [{}]", output_path));
+        let mut expected_event: Event = serde_json::from_str(&output_json)
+            .map_err(|e| panic!("Cannot parse output json. Err: {}", e))
+            .unwrap();;
+
+        // Act
+        let result = collector.to_event(&input_json);
+
+        // Assert
+        assert!(result.is_ok());
+
+        let result_event = result.unwrap();
+        expected_event.created_ts = result_event.created_ts.clone();
+
+        assert_eq!(expected_event, result_event);
     }
 
 }
