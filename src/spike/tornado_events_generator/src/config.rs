43e620019cbfbf851f231e363ee8ffb7ff992a58
Francesco Cina <mr.francesco.cina@gmail.com>
2019-02-20 11:05:40 +0100

    [tornado/tornado] Reconfigure tornado_events_generator spike

--- src/spike/tornado_events_generator/src/config.rs
+++ src/spike/tornado_events_generator/src/config.rs
@@ -4,9 +4,15 @@ use tornado_common_logger::LoggerConfig;
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub struct Io {
-    /// The filesystem folder where Events are saved in JSON format
-    #[structopt(long, default_value = "./events")]
-    pub json_events_path: String,
+
+    /// The filesystem folder where the Tornado configuration is saved
+    #[structopt(long, default_value = "/etc/tornado/tornado_events_generator")]
+    pub config_dir: String,
+
+    /// The filesystem folder where Events are saved in JSON format;
+    ///   this folder is relative to the `config_dir`.
+    #[structopt(long, default_value = "/events")]
+    pub events_dir: String,
 
     /// The Unix Socket path where outgoing events will be written
     #[structopt(long, default_value = "/var/run/tornado/tornado.sock")]
@@ -15,6 +21,10 @@ pub struct Io {
     /// How many times each event should be sent
     #[structopt(long, default_value = "1000")]
     pub repeat_send: usize,
+
+    /// How long to sleep after each event is sent, in milliseconds
+    #[structopt(long, default_value = "2000")]
+    pub repeat_sleep_ms: u64,
 }
 
 #[derive(Debug, StructOpt)]
