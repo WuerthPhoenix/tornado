c5b88177113d47c768e4f9da20d42516a590bc7d
Francesco Cina <mr.francesco.cina@gmail.com>
2019-04-24 12:36:59 +0200

    frontend stub

--- src/ui/frontend/build.sh
+++ src/ui/frontend/build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+
+SRC="../dto/pkg/dto.d.ts"
+DEST_DIR="./src/generated"
+
+rm -rf $DEST_DIR
+mkdir $DEST_DIR
+cp -r $SRC $DEST_DIR
+
+npm run lint && \
+npm run test:unit && \
+npm run build
