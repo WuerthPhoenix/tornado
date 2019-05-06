2e800c879929fe31d369ed1c4ba7005eb96a2436
Francesco Cina <mr.francesco.cina@gmail.com>
2019-05-06 15:35:06 +0200

    use tornado specific frontend node id

--- src/ui/frontend/src/main.ts
+++ src/ui/frontend/src/main.ts
@@ -5,8 +5,8 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-new Vue({
+const tornadoApp = new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#tornado-app');
