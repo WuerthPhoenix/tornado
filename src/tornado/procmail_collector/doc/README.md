cf3cf13669ecd89daf41ec5df578a5ae07c05773
Francesco Cina <mr.francesco.cina@gmail.com>
2019-05-20 17:30:15 +0200

    [tornado/procmail_collector] Add project stub

---
+++ src/tornado/procmail_collector/doc/README.md
@@ -0,0 +1,10 @@
+# Tornado Procmail Collector (executable)
+
+The Procmail Collector binary is an executable that generates Tornado Events from
+procmail inputs.
+
+
+
+## How It Works
+
+ToDo
---
+++ src/tornado/procmail_collector/src/main.rs
@@ -0,0 +1,55 @@
+pub mod actors;
+pub mod config;
+
+use actix::prelude::*;
+use failure::Fail;
+use log::*;
+use std::io::{stdin, BufRead};
+use std::thread;
+use tornado_common::actors::message::StringMessage;
+use tornado_common_logger::setup_logger;
+use tornado_common::actors::uds_server::listen_to_uds_socket;
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let conf = config::Conf::build();
+
+    // Setup logger
+    setup_logger(&conf.logger).map_err(|err| err.compat())?;
+
+    info!("Procmail collector started");
+
+    // start system
+    System::run(move || {
+
+        // Start TcpWriter
+        let tornado_tcp_address =
+            format!("{}:{}", conf.io.tornado_event_socket_ip, conf.io.tornado_event_socket_port);
+        let tpc_client_addr = tornado_common::actors::tcp_client::TcpClientActor::start_new(
+            tornado_tcp_address.clone(),
+            conf.io.message_queue_size,
+        );
+
+        // Start Procmail collector
+        /*
+        let rsyslog_addr = SyncArbiter::start(1, move || {
+            actors::sync_collector::ProcmailCollectorActor::new(tpc_client_addr.clone())
+        });
+        */
+
+        // Open UDS socket
+        listen_to_uds_socket(conf.io.uds_path.clone(), move |msg| {
+            info!("Received message on the socket")
+        })
+            .and_then(|_| {
+                info!("Started UDS server at [{}]. Listening for incoming events", conf.io.uds_path.clone());
+                Ok(())
+            })
+            .unwrap_or_else(|err| {
+                error!("Cannot start UDS server at [{}]. Err: {}", conf.io.uds_path.clone(), err);
+                std::process::exit(1);
+            });
+
+    })?;
+
+    Ok(())
+}
