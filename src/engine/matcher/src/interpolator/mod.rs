4750310b9419f0bd92580f00281d47ce6ed06cd3
Francesco Cina <mr.francesco.cina@gmail.com>
2019-06-19 10:38:57 +0200

    Add string interpolator template split

--- src/engine/matcher/src/interpolator/mod.rs
+++ src/engine/matcher/src/interpolator/mod.rs
@@ -1,7 +1,8 @@
 //
-// For performance considerations check:
+// For performance considerations see:
 // - https://lise-henry.github.io/articles/optimising_strings.html
 // - https://users.rust-lang.org/t/fast-string-concatenation/4425
+// - https://github.com/hoodie/concatenation_benchmarks-rs
 //
 
 use lazy_static::*;
@@ -33,6 +34,58 @@ impl StringInterpolator {
         Ok(StringInterpolator {
             accessors
         })
+    }
+
+    fn split(template: &str) -> Result<Vec<&str>, MatcherError> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"(\$\{[^}]+})").unwrap();
+        }
+
+
+        let matches: Vec<(usize, usize)> = RE.find_iter(template)
+            .map(|m| (m.start(), m.end()))
+            .collect();
+
+        // get index of first arg match or return a copy of the template if no args matched
+        let first = match matches.first() {
+            Some((start, _)) => *start,
+            _ => return Ok(vec![template]),
+        };
+
+        let mut parts = vec![];
+
+        // copy from template start to first arg
+        if first > 0 {
+            parts.push(&template[0..first])
+        }
+
+        // keeps the index of the previous argument end
+        let mut prev_end: Option<usize> = None;
+
+        // loop all matches
+        for (start, end) in matches.iter() {
+            // copy from previous argument end till current argument start
+            if let Some(last_end) = prev_end {
+                parts.push(&template[last_end..*start])
+            }
+
+            // argument name with braces
+            parts.push(&template[*start..*end]);
+
+            prev_end = Some(*end);
+        }
+
+        let template_len = template.len();
+
+        // if last arg end index isn't the end of the string then copy
+        // from last arg end till end of template
+        if let Some(last_pos) = prev_end {
+            if last_pos < template_len {
+                parts.push(&template[last_pos..template_len])
+            }
+        }
+
+        Ok(parts)
     }
 
 }
@@ -98,5 +151,25 @@ mod test {
             Accessor::Constant {value} => assert_eq!("</span></${}div>", value),
             _ => assert!(false)
         }
+    }
+
+    #[test]
+    fn should_split_based_on_expressions_delimiters() {
+        // Arrange
+        let template = "<div><span>${event.payload.test}</sp${event.type}an><span>${_variables.test12}</span></${}div>";
+
+        // Act
+        let parts = StringInterpolator::split(template).unwrap();
+
+        // Assert
+        assert_eq!(7, parts.len());
+        assert_eq!("<div><span>", parts[0]);
+        assert_eq!("${event.payload.test}", parts[1]);
+        assert_eq!("</sp", parts[2]);
+        assert_eq!("${event.type}", parts[3]);
+        assert_eq!("an><span>", parts[4]);
+        assert_eq!("${_variables.test12}", parts[5]);
+        assert_eq!("</span></${}div>", parts[6]);
+
     }
 }
