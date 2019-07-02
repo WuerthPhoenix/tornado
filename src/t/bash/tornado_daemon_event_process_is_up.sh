c480abef19b4d63b0f46554a824fa1ad95f6e532
Francesco Cina <mr.francesco.cina@gmail.com>
2019-07-02 12:05:24 +0200

    Add Integration Test for event sending to tornado daemin

--- src/t/bash/tornado_daemon_event_process_is_up.sh
+++ src/t/bash/tornado_daemon_event_process_is_up.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-HTTP_RESPONSE_CODE=$(curl -sS -k -o /dev/null -w "%{http_code}" -H "content-type: application/json" -X POST -d '{"event":{"type":"something", "created_ms":111, "payload": {}}, "process_type":"SkipActions"}' http://localhost:4748/api/send_event)
+TEXT="NotMatched"
 
-if [ $HTTP_RESPONSE_CODE -ne 200 ]; then
-    echo "[-] FAILED: Tornado daemon event sending check failed, response code was: $HTTP_RESPONSE_CODE"
-    exit 1
-else
+if curl -H "content-type: application/json" -X POST -d '{"event":{"type":"something", "created_ms":111, "payload": {}}, "process_type":"SkipActions"}' http://localhost:4748/api/send_event | grep -q "$TEXT"; then
     echo "Tornado daemon event sending check succeeded"
-fi 
+else
+    echo "[-] FAILED: Tornado daemon event sending check failed"
+    exit 1
+fi
