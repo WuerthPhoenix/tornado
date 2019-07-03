450c48c2a1aa125e376471c3579fbc37b7e596c7
karlis upitis <karlis@upitis.com>
2019-07-03 13:28:41 +0300

    Include ds module, its style and init the plugin

--- src/ui/frontend/src/main.ts
+++ src/ui/frontend/src/main.ts
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import DesignSystem from 'wp-design-system';
+import 'wp-design-system/dist/system/system.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(DesignSystem);
 const tornadoApp = new Vue({
   router,
   store,
