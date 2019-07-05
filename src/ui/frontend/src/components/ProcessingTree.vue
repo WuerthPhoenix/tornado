f983fb5101790e1edfbbc2bc96f6d35f875ac29a
Francesco Cina <mr.francesco.cina@gmail.com>
2019-07-05 16:56:32 +0200

    [ui/frontend] Add tornado config to TreeData recursive convertion

--- src/ui/frontend/src/components/ProcessingTree.vue
+++ src/ui/frontend/src/components/ProcessingTree.vue
@@ -7,6 +7,7 @@
     </header>
     <div class="card-content">
       <div class="content">
+        <Tree :data="[treeData]" />
         <DisplayAsJson :obj="tree"/>
       </div>
     </div>
@@ -17,6 +18,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import DisplayAsJson from '@/components/DisplayAsJson.vue';
 import { MatcherConfigDto } from '../generated/dto';
+import { Patterns } from 'wp-design-system';
 
 @Component({
   components: {
@@ -25,5 +27,40 @@ import { MatcherConfigDto } from '../generated/dto';
 })
 export default class ProcessingTree extends Vue {
   @Prop() public tree!: MatcherConfigDto;
+
+  get treeData(): Patterns.TreeData {
+    const treeNode: Patterns.TreeNode = this.toTreeNode('root', this.tree);
+    return {
+      active: 0,
+      nodes: [treeNode],
+    };
+  }
+
+  private toTreeNode(name: string, node: MatcherConfigDto): Patterns.TreeNode {
+    if (node.type === 'Filter') {
+      const treeNode: Patterns.TreeNode = {
+        id: 0,
+        title: `Filter - ${name}` + node.filter.name,
+        description: node.filter.description,
+        actions: [],
+        children: [],
+      };
+
+      Object.keys(node.nodes).forEach((key) => {
+        treeNode.children.push(this.toTreeNode(key, node.nodes[key]));
+      });
+
+      return treeNode;
+    } else {
+      const treeNode: Patterns.TreeNode = {
+        id: 0,
+        title: `Rules - ${name}`,
+        description: `Rule set with ${node.rules.length} rules`,
+        actions: [],
+        children: [],
+      };
+      return treeNode;
+    }
+  }
 }
 </script>
