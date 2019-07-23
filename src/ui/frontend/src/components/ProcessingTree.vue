0ca70383b963eeb0aff8f9f170bde15841fd8cdc
Francesco Cina <mr.francesco.cina@gmail.com>
2019-07-23 10:19:54 +0200

    Configure API path to work in IcingaWeb2 tornado module

--- src/ui/frontend/src/components/ProcessingTree.vue
+++ src/ui/frontend/src/components/ProcessingTree.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <Tree :data="treeData" :key="Math.random()"/>
-    <!-- <DisplayAsJson :obj="treeData"/> -->
+    <Tree class="wptree" :data="treeData" :key="Math.random()"/>
+    <DisplayAsJson :obj="treeData"/>
   </div>
 </template>
 
@@ -22,17 +22,17 @@ export default class ProcessingTree extends Vue {
   public count = 0;
 
   get treeData(): Patterns.TreeData {
-    const treeCard: Patterns.TreeCard = this.toTreeNode('root', this.count++, this.tree);
+    const treeCard: Patterns.TreeCard = this.toTreeNode('root', this.tree);
     // console.log(`count: ${this.count}`);
     // console.log(treeCard);
     return {cards: [treeCard]};
   }
 
-  private toTreeNode(name: string, id: number, node: MatcherConfigDto): Patterns.TreeCard {
+  private toTreeNode(name: string, node: MatcherConfigDto): Patterns.TreeCard {
     if (node.type === 'Filter') {
       const treeNode: Patterns.TreeCard = {
         active: false,
-        id,
+        id: this.count++,
         title: `${name} ${node.filter.name}`,
         description: node.filter.description,
         actions: [],
@@ -40,14 +40,13 @@ export default class ProcessingTree extends Vue {
       };
 
       Object.keys(node.nodes).forEach((key) => {
-        id = id + 1;
-        treeNode.children.push(this.toTreeNode(key, id, node.nodes[key]));
+        treeNode.children.push(this.toTreeNode(key, node.nodes[key]));
       });
 
       return treeNode;
     } else {
       const treeNode: Patterns.TreeCard = {
-        id,
+        id: this.count++,
         active: false,
         title: `${name}`,
         description: ``,
