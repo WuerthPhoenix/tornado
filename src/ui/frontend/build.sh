433270f520e438dc28529e1cf9d40eec8b09e44b
karlis upitis <karlis@upitis.com>
2019-07-31 11:04:22 +0300

    Split build task in two

--- src/ui/frontend/build.sh
+++ src/ui/frontend/build.sh
@@ -9,10 +9,4 @@ rm -rf $DEST_DIR
 mkdir $DEST_DIR
 cp -r $SRC $DEST_DIR$DEST
 
-rm -rf dist/ && \
-rm -rf node_modules/ && \
-npm install && \
-npm run lint && \
-npm run test:unit && \
-npm run test:e2e-headless && \
-npm run build
+./build-ui.sh
