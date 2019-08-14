f92546b8119c53c9adb02cc9cba5cdbea4affb07
Francesco Cina <mr.francesco.cina@gmail.com>
2019-08-14 13:57:36 +0200

    icinga2 collector configuration moved into toml file

--- src/tornado/icinga2_collector/src/main.rs
+++ src/tornado/icinga2_collector/src/main.rs
@@ -13,12 +13,11 @@ mod error;
 fn main() -> Result<(), Box<std::error::Error>> {
     let config = config::Conf::build();
 
-    let icinga2_config_path = format!("{}/{}", &config.io.config_dir, "icinga2_collector.toml");
-    let icinga2_config = config::build_config(&icinga2_config_path)?;
+    let icinga2_config = config::build_config(&config.config_dir)?;
 
     setup_logger(&icinga2_config.logger).map_err(failure::Fail::compat)?;
 
-    let streams_dir = format!("{}/{}", &config.io.config_dir, &config.io.streams_dir);
+    let streams_dir = format!("{}/{}", &config.config_dir, &config.streams_dir);
     let streams_config =
         config::read_streams_from_config(&streams_dir).map_err(failure::Fail::compat)?;
 
@@ -27,10 +26,10 @@ fn main() -> Result<(), Box<std::error::Error>> {
 
         let tornado_tcp_address = format!(
             "{}:{}",
-            config.io.tornado_event_socket_ip, config.io.tornado_event_socket_port
+            icinga2_config.icinga2_collector.tornado_event_socket_ip, icinga2_config.icinga2_collector.tornado_event_socket_port
         );
         let tcp_client_addr =
-            TcpClientActor::start_new(tornado_tcp_address.clone(), config.io.message_queue_size);
+            TcpClientActor::start_new(tornado_tcp_address.clone(), icinga2_config.icinga2_collector.message_queue_size);
 
         streams_config.iter().for_each(|config| {
             let config = config.clone();
@@ -39,7 +38,7 @@ fn main() -> Result<(), Box<std::error::Error>> {
             SyncArbiter::start(1, move || {
                 let tcp_client_addr = tcp_client_addr.clone();
                 actor::Icinga2StreamActor {
-                    icinga_config: icinga2_config.icinga2_collector.clone(),
+                    icinga_config: icinga2_config.icinga2_collector.connection.clone(),
                     collector: JMESPathEventCollector::build(config.collector_config.clone())
                         .unwrap_or_else(|e| panic!("Not able to start JMESPath collector with configuration: \n{:#?}. Err: {}", config.collector_config.clone(), e)),
                     stream_config: config.stream.clone(),
