15b86cb9ab6caa3b1b0d9acf2811e18fe720a027
Francesco Cina <mr.francesco.cina@gmail.com>
2019-08-28 16:02:57 +0200

    fix clippy warns

--- src/spike/dynamic_log/src/main.rs
+++ src/spike/dynamic_log/src/main.rs
@@ -2,7 +2,6 @@ use slog::*;
 use slog_async::Async;
 
 fn main() {
-
     let debug_drain = new_drain(Level::Debug);
     let info_drain = new_drain(Level::Info);
     let warn_drain = new_drain(Level::Warning);
@@ -17,8 +16,8 @@ fn main() {
 
     println!("------------------");
     debug!(log, "debug log 1"); // this is logged
-    info!(log, "info log 1");   // this is logged
-    warn!(log, "warn log 1");   // this is logged
+    info!(log, "info log 1"); // this is logged
+    warn!(log, "warn log 1"); // this is logged
 
     // Change the log level at runtime.
     // Now the global log level is INFO
@@ -26,8 +25,8 @@ fn main() {
 
     println!("------------------");
     debug!(log, "debug log 2"); // this is NOT logged anymore
-    info!(log, "info log 2");   // this is logged
-    warn!(log, "warn log 2");   // this is logged
+    info!(log, "info log 2"); // this is logged
+    warn!(log, "warn log 2"); // this is logged
 
     // Change the log level at runtime.
     // Now the global log level is WARN
@@ -35,15 +34,13 @@ fn main() {
 
     println!("------------------");
     debug!(log, "debug log 3"); // this is NOT logged anymore
-    info!(log, "info log 3");   // this is NOT logged anymore
-    warn!(log, "warn log 3");   // this is logged
+    info!(log, "info log 3"); // this is NOT logged anymore
+    warn!(log, "warn log 3"); // this is logged
 }
 
 fn new_drain(level: Level) -> Fuse<LevelFilter<Fuse<Async>>> {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
-
     drain.filter_level(level).fuse()
-
 }
