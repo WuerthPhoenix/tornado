6b8d7f57aa4f9b844351fe770832315ea1948b57
Francesco Cina <mr.francesco.cina@gmail.com>
2020-02-12 11:44:05 +0100

    Fix wrong webhook_collector startup

--- tornado/webhook_collector/src/main.rs
+++ tornado/webhook_collector/src/main.rs
@@ -64,7 +64,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
         error!("Server cannot start on port {}. Err: {}", port, err);
         std::process::exit(1);
     })
-    .run();
+    .run().await?;
 
     Ok(())
 }
