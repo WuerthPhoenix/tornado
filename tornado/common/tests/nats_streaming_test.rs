4f363fc5a695dcda4f968e9f6fa0709cfcfa00c9
Francesco Cina <mr.francesco.cina@gmail.com>
2020-02-18 12:15:24 +0100

    Add NATS streaming subscriber stub - add tests

--- tornado/common/tests/nats_streaming_test.rs
+++ tornado/common/tests/nats_streaming_test.rs
@@ -3,6 +3,8 @@ use tornado_common::actors::nats_streaming_publisher::NatsPublisherActor;
 use rants::Client;
 use futures::stream::StreamExt;
 use tornado_common::actors::message::EventMessage;
+use tornado_common::actors::nats_streaming_subscriber::subscribe_to_nats_streaming;
+use std::sync::{Arc, Mutex};
 
 const BASE_ADDRESS: &str = "127.0.0.1:4222";
 
@@ -10,15 +12,20 @@ const BASE_ADDRESS: &str = "127.0.0.1:4222";
 async fn should_publish_to_nats_streaming() {
     let subject = "test_subject";
 
-    let subscriber = Client::new(vec![BASE_ADDRESS.parse().unwrap()]);
-    subscriber.connect().await;
-    let (_, mut subscription) = subscriber.subscribe(&subject.parse().unwrap(), 1024).await.unwrap();
+    let received = Arc::new(Mutex::new(None));
+
+    let received_clone = received.clone();
+    subscribe_to_nats_streaming(BASE_ADDRESS, subject, move |event| {
+        let mut lock = received_clone.lock().unwrap();
+        *lock = Some(event.event);
+        Ok(())
+    }).await.unwrap();
 
     let publisher = NatsPublisherActor::start_new(BASE_ADDRESS, subject, 10).await;
     publisher.do_send(EventMessage { event: Event::new("an_event") });
 
-    // Read a message from the subscription
-    let message = subscription.next().await.unwrap();
-    let message = String::from_utf8(message.into_payload()).unwrap();
-    println!("Received '{}'", message);
+    let event = received.lock().unwrap();
+    assert!(event.is_some());
+    assert_eq!("an_event", event.as_ref().unwrap().event_type);
+
 }
