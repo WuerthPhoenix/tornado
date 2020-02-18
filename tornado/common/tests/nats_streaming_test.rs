af7129e2d8807f39559a2cb194cdced2126196bf
Francesco Cina <mr.francesco.cina@gmail.com>
2020-02-18 12:24:23 +0100

    Add NATS streaming subscriber stub - add tests

--- tornado/common/tests/nats_streaming_test.rs
+++ tornado/common/tests/nats_streaming_test.rs
@@ -5,11 +5,13 @@ use futures::stream::StreamExt;
 use tornado_common::actors::message::EventMessage;
 use tornado_common::actors::nats_streaming_subscriber::subscribe_to_nats_streaming;
 use std::sync::{Arc, Mutex};
+use tokio::time;
 
 const BASE_ADDRESS: &str = "127.0.0.1:4222";
 
 #[actix_rt::test]
 async fn should_publish_to_nats_streaming() {
+    let random =
     let subject = "test_subject";
 
     let received = Arc::new(Mutex::new(None));
@@ -23,6 +25,8 @@ async fn should_publish_to_nats_streaming() {
 
     let publisher = NatsPublisherActor::start_new(BASE_ADDRESS, subject, 10).await;
     publisher.do_send(EventMessage { event: Event::new("an_event") });
+
+    time::delay_until(time::Instant::now() + time::Duration::new(2, 0)).await;
 
     let event = received.lock().unwrap();
     assert!(event.is_some());
