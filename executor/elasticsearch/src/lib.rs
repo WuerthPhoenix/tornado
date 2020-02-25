fb22c221672cb2296927a39a35880e96b9580af2
Damiano Chini <damiano.chini@wuerth-phoenix.com>
2020-02-25 17:11:41 +0100

    Add draft implementation for es executor with reqwest

--- executor/elasticsearch/src/lib.rs
+++ executor/elasticsearch/src/lib.rs
@@ -1,6 +1,7 @@
 use log::*;
-use tornado_common_api::{Action, Value};
+use tornado_common_api::{Action};
 use tornado_executor_common::{Executor, ExecutorError};
+use reqwest::Client;
 
 /// An executor that sends data to elasticsearch
 #[derive(Default)]
@@ -23,18 +24,24 @@ impl Executor for ElasticsearchExecutor {
     fn execute(&mut self, action: Action) -> Result<(), ExecutorError> {
         trace!("ElasticsearchExecutor - received action: \n[{:?}]", action);
 
-        Ok(())
-    }
-}
-
-impl ElasticsearchExecutor {
-    fn execute_async(&self, action: Action) -> Result<(), ExecutorError> {
         let data = action.payload.get("data").ok_or_else(|| {
             ExecutorError::MissingArgumentError { message: "data field is missing".to_string() }
         })?;
-        let endpoint = "http://127.0.0.1:9200";
 
-        Ok(())
+        let index_name = "tornado";
+        let endpoint = format!("http://127.0.0.1:9200/{}/_doc/", index_name);
+
+        let client = Client::new();
+        let res = client.post(&endpoint)
+            .json(&data)
+            .send().map_err(|err| ExecutorError::ActionExecutionError { message: format!("Error while sending document to Elasticsearch. Err: {}", err) })?;
+
+        if !res.status().is_success(){
+            Err(ExecutorError::ActionExecutionError { message: format!("Error while sending document to Elasticsearch. Response: {:?}", res) })
+        } else {
+            Ok(())
+        }
+
     }
 }
 
@@ -42,15 +49,22 @@ impl ElasticsearchExecutor {
 mod test {
     use super::*;
     use std::collections::HashMap;
+    use tornado_common_api::Value;
 
     #[test]
     fn should_send_document_to_elasticsearch() {
         // Arrange
-        let executor = ElasticsearchExecutor {};
+        let mut executor = ElasticsearchExecutor {};
         let mut action = Action { id: "elasticsearch".to_string(), payload: HashMap::new() };
-        action.payload.insert("data".to_owned(), Value::Text("ciao elasticsearch".to_owned()));
+        let mut es_document = HashMap::new();
+        es_document.insert("message".to_owned(), Value::Text("message to elasticsearch".to_owned()));
+        es_document.insert("user".to_owned(), Value::Text("myuser".to_owned()));
+
+        action.payload.insert("data".to_owned(), Value::Map(es_document));
 
         // Act
-        let result = executor.execute_async(action);
+        let result = executor.execute(action);
+
+        assert_eq!(result, Ok(()));
     }
 }
