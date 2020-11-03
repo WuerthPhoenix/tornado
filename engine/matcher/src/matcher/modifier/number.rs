103d5e6be9c87d80a93f7a4ad84a7a5316318ea6
Francesco Cina <mr.francesco.cina@gmail.com>
2020-11-03 15:38:39 +0100

    Add to_number modifier stub

--- engine/matcher/src/matcher/modifier/number.rs
+++ engine/matcher/src/matcher/modifier/number.rs
@@ -3,6 +3,15 @@ use tornado_common_api::Value;
 
 #[inline]
 pub fn to_number(variable_name: &str, value: &mut Value) -> Result<(), MatcherError> {
-    let to_do = 1;
-    unimplemented!()
+    match value {
+        Value::Text(text) => {
+            //*value = Value::Text(trimmed.to_owned());
+            Ok(())
+        }
+        Value::Number(..) => Ok(()),
+        _ => Err(MatcherError::ExtractedVariableError {
+            message: "The 'to_number' modifier can be used only with values of type 'string' or 'number'".to_owned(),
+            variable_name: variable_name.to_owned(),
+        })
+    }
 }
