a51b94376ff8f1151ff3e8a84d8f00fed0a59434
Francesco Cina <mr.francesco.cina@gmail.com>
2020-11-23 11:42:55 +0100

    Actions inside foreach should be migrated

--- tornado/engine/src/command/upgrade_rules.rs
+++ tornado/engine/src/command/upgrade_rules.rs
@@ -1,7 +1,11 @@
-use crate::command::daemon::{ACTION_ID_MONITORING, ACTION_ID_SMART_MONITORING_CHECK_RESULT};
+use crate::command::daemon::{
+    ACTION_ID_FOREACH, ACTION_ID_MONITORING, ACTION_ID_SMART_MONITORING_CHECK_RESULT,
+};
 use crate::config::parse_config_files;
+use tornado_common_api::Value;
 use tornado_engine_matcher::config::rule::Action;
 use tornado_engine_matcher::config::{MatcherConfig, MatcherConfigEditor, MatcherConfigReader};
+use tornado_engine_matcher::error::MatcherError;
 
 pub fn upgrade_rules(
     config_dir: &str,
@@ -34,22 +38,8 @@ fn upgrade(
         MatcherConfig::Ruleset { name: _, rules } => {
             for rule in rules {
                 for action in &mut rule.actions {
-                    if action.id == ACTION_ID_MONITORING {
-                        println!(
-                            "Migrating {} action to {}",
-                            ACTION_ID_MONITORING, ACTION_ID_SMART_MONITORING_CHECK_RESULT
-                        );
-                        match tornado_executor_smart_monitoring_check_result::migration::migrate_from_monitoring(&action.payload) {
-                            Ok(migrated_payload) => {
-                                *action = Action {
-                                    id: ACTION_ID_SMART_MONITORING_CHECK_RESULT.to_owned(),
-                                    payload: migrated_payload
-                                }
-                            },
-                            Err(err) => {
-                                println!("Error Migrating {} action to {}. Err: {}", ACTION_ID_MONITORING, ACTION_ID_SMART_MONITORING_CHECK_RESULT, err);
-                            }
-                        };
+                    if let Err(err) = upgrade_action(action) {
+                        println!("Error Migrating {}. Err: {}", action.id, err);
                     }
                 }
             }
@@ -58,10 +48,60 @@ fn upgrade(
     Ok(())
 }
 
+fn upgrade_action(
+    action: &mut Action,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    if action.id == ACTION_ID_MONITORING {
+        println!(
+            "Migrating {} action to {}",
+            ACTION_ID_MONITORING, ACTION_ID_SMART_MONITORING_CHECK_RESULT
+        );
+        match tornado_executor_smart_monitoring_check_result::migration::migrate_from_monitoring(
+            &action.payload,
+        ) {
+            Ok(migrated_payload) => {
+                *action = Action {
+                    id: ACTION_ID_SMART_MONITORING_CHECK_RESULT.to_owned(),
+                    payload: migrated_payload,
+                };
+                Ok(())
+            }
+            Err(err) => Err(err.into()),
+        }
+    } else if action.id == ACTION_ID_FOREACH {
+        println!("Migrating {} inner actions", ACTION_ID_FOREACH);
+        if let Some(Value::Array(inner_actions)) = action.payload.get_mut("actions") {
+            for inner_action in inner_actions {
+                let mut new_action = value_to_action(&inner_action)?;
+                upgrade_action(&mut new_action)?;
+                *inner_action = new_action.into();
+            }
+        }
+        Ok(())
+    } else {
+        Ok(())
+    }
+}
+
+fn value_to_action(value: &Value) -> Result<Action, MatcherError> {
+    let option_id = value.get_map().and_then(|map| map.get("id")).and_then(|id| id.get_text());
+    let option_payload =
+        value.get_map().and_then(|map| map.get("payload")).and_then(|id| id.get_map());
+    if let (Some(id), Some(payload)) = (option_id, option_payload) {
+        Ok(Action { id: id.to_owned(), payload: payload.clone() })
+    } else {
+        Err(MatcherError::ConfigurationError {
+            message: "foreach actions in payload must have 'id' and 'payload'".to_owned(),
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::command::daemon::{ACTION_ID_FOREACH, ACTION_ID_LOGGER};
     use tempfile::TempDir;
+    use tornado_engine_matcher::config::rule::Rule;
 
     #[test]
     fn should_migrate_the_monitoring_rules() {
@@ -80,6 +120,40 @@ mod test {
         assert_ne!(matcher_config_before, matcher_config_after);
 
         check_migrated_rules(&matcher_config_before, &matcher_config_after);
+    }
+
+    #[test]
+    fn should_migrate_the_monitoring_rules_inside_foreach() {
+        // Arrange
+        let filename = "./config/rules.d/ruleset_01/080_monitoring_foreach.json";
+        let json = std::fs::read_to_string(filename)
+            .expect(&format!("Unable to open the file [{}]", filename));
+        let foreach_action = Rule::from_json(&json).unwrap().actions[0].clone();
+
+        let mut migrated_action = foreach_action.clone();
+
+        // Act
+        upgrade_action(&mut migrated_action).unwrap();
+
+        // Assert
+        assert_eq!(foreach_action.id, ACTION_ID_FOREACH);
+        assert_eq!(ACTION_ID_LOGGER, get_foreach_inner_action_id(&foreach_action, 0));
+        assert_eq!(ACTION_ID_MONITORING, get_foreach_inner_action_id(&foreach_action, 1));
+
+        assert_eq!(migrated_action.id, ACTION_ID_FOREACH);
+        assert_eq!(ACTION_ID_LOGGER, get_foreach_inner_action_id(&migrated_action, 0));
+        assert_eq!(
+            ACTION_ID_SMART_MONITORING_CHECK_RESULT,
+            get_foreach_inner_action_id(&migrated_action, 1)
+        );
+    }
+
+    fn get_foreach_inner_action_id(foreach_action: &Action, position: usize) -> &str {
+        foreach_action.payload.get("actions").unwrap().get_array().unwrap()[position]
+            .get_map()
+            .unwrap()["id"]
+            .get_text()
+            .unwrap()
     }
 
     fn prepare_temp_dirs(tempdir: &TempDir) -> (String, String, String) {
@@ -149,7 +223,7 @@ mod test {
                                                 after_action.id
                                             );
                                             assert_ne!(before_action, after_action)
-                                        } else {
+                                        } else if before_action.id != ACTION_ID_FOREACH {
                                             assert_eq!(before_action, after_action);
                                         }
                                     }
