6e68cac30cf93883729926b959e6afd0ba29a84c
Damiano Chini <damiano.chini@wuerth-phoenix.com>
2021-07-28 14:38:36 +0200

    Move Apm config in separate module

--- common/logger/src/lib.rs
+++ common/logger/src/lib.rs
@@ -1,15 +1,17 @@
+use crate::elastic_apm::{
+    get_current_service_name, ApiCredentials, ApmTracingConfig,
+    DEFAULT_APM_SERVER_CREDENTIALS_FILENAME,
+};
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::BufReader;
 use std::str::FromStr;
 use thiserror::Error;
 use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_appender::non_blocking::WorkerGuard;
-use tracing_elastic_apm::config::{ApiKey, Authorization};
+use tracing_elastic_apm::config::Authorization;
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, EnvFilter, Registry};
 
-const DEFAULT_APM_SERVER_CREDENTIALS_FILENAME: &str = "apm_server_api_credentials.json";
+mod elastic_apm;
 
 /// Defines the Logger configuration.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -29,55 +31,6 @@ pub struct LoggerConfig {
 
     #[serde(default)]
     pub tracing_elastic_apm: ApmTracingConfig,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ApmTracingConfig {
-    // The url of the Elastic APM server; if provided, traces will be sent to this server;
-    // if not provided traces will not be sent.
-    pub apm_server_url: Option<String>,
-
-    // The path of file containing credentials for calling the APM server APIs;
-    pub apm_server_credentials_filepath: Option<String>,
-}
-
-impl Default for ApmTracingConfig {
-    fn default() -> Self {
-        Self { apm_server_url: None, apm_server_credentials_filepath: None }
-    }
-}
-
-#[derive(Debug, Deserialize, PartialEq)]
-struct ApiCredentials {
-    id: String,
-    key: String,
-}
-
-impl ApiCredentials {
-    fn from_file(apm_server_credentials_filepath: &str) -> Result<Self, LoggerError> {
-        let apm_server_credentials_file = File::open(&apm_server_credentials_filepath)?;
-        let apm_server_credentials_reader = BufReader::new(apm_server_credentials_file);
-
-        serde_json::from_reader(apm_server_credentials_reader).map_err(|err| {
-            LoggerError::LoggerConfigurationError {
-                message: format!(
-                    "Failed to read APM server Api Key from file {}. Error: {:?}",
-                    &apm_server_credentials_filepath, err
-                ),
-            }
-        })
-    }
-}
-
-fn get_current_service_name() -> Result<String, LoggerError> {
-    let current_executable = std::env::current_exe()?;
-    let filename = current_executable
-        .file_name()
-        .and_then(|filename_os_str| filename_os_str.to_str())
-        .map(|filename_str| filename_str.to_string());
-    filename.ok_or(LoggerError::LoggerRuntimeError {
-        message: "Could not get current executable file name".to_string(),
-    })
 }
 
 #[derive(Error, Debug)]
@@ -175,18 +128,15 @@ pub fn setup_logger(
             {
                 apm_server_credentials_filepath
             } else {
-                format!("{}/{}", config_dir, self::DEFAULT_APM_SERVER_CREDENTIALS_FILENAME)
+                format!("{}/{}", config_dir, DEFAULT_APM_SERVER_CREDENTIALS_FILENAME)
             };
-            let apm_server_api_credentials = ApiCredentials::from_file(&apm_server_credentials_filepath)?;
+            let apm_server_api_credentials =
+                ApiCredentials::from_file(&apm_server_credentials_filepath)?;
 
             Some(tracing_elastic_apm::new_layer(
                 get_current_service_name()?,
-                tracing_elastic_apm::config::Config::new(apm_server_url).with_authorization(
-                    Authorization::ApiKey(ApiKey::new(
-                        apm_server_api_credentials.id,
-                        apm_server_api_credentials.key,
-                    )),
-                ),
+                tracing_elastic_apm::config::Config::new(apm_server_url)
+                    .with_authorization(Authorization::ApiKey(apm_server_api_credentials.into())),
             ))
         } else {
             None
@@ -231,7 +181,6 @@ where
 
 #[cfg(test)]
 mod test {
-
     use super::*;
 
     #[test]
@@ -275,33 +224,5 @@ mod test {
     #[test]
     fn split_the_file_path_should_file_if_directory_is_not_present() {
         assert!(path_to_dir_and_filename("filename").is_err());
-    }
-
-    #[test]
-    fn should_get_correct_service_name() {
-        assert!(get_current_service_name().unwrap().starts_with("tornado_common_logger"));
-    }
-
-    #[test]
-    fn should_read_api_credentials_correct_file() {
-        let api_credentials =
-            ApiCredentials::from_file("./test_resources/apm_server_api_credentials.json").unwrap();
-        assert_eq!(
-            api_credentials,
-            ApiCredentials { id: "myid".to_string(), key: "mykey".to_string() }
-        );
-    }
-
-    #[test]
-    fn should_read_api_credentials_should_return_error_if_file_does_not_exist() {
-        let res = ApiCredentials::from_file("./non-existing.json");
-        assert!(res.is_err());
-    }
-
-    #[test]
-    fn should_read_api_credentials_should_return_error_if_file_is_not_correcly_formatted() {
-        let res =
-            ApiCredentials::from_file("./test_resources/apm_server_api_credentials_wrong.json");
-        assert!(res.is_err());
     }
 }
