42f888d55c5eaf3880469913150d03aac913fca4
Francesco Cina <mr.francesco.cina@gmail.com>
2021-08-25 12:09:00 +0200

    Remove clap based tornado config

--- tornado/engine/src/config.rs
+++ tornado/engine/src/config.rs
@@ -15,37 +15,9 @@ use tornado_executor_director::config::DirectorClientConfig;
 use tornado_executor_elasticsearch::config::ElasticsearchConfig;
 use tornado_executor_icinga2::config::Icinga2ClientConfig;
 use tornado_executor_smart_monitoring_check_result::config::SmartMonitoringCheckResultConfig;
-use structopt::clap::{App, Arg, ArgMatches};
 use structopt::StructOpt;
 
 pub const CONFIG_DIR_DEFAULT: Option<&'static str> = option_env!("TORNADO_CONFIG_DIR_DEFAULT");
-
-pub const SUBCOMMAND_CHECK: &str = "check";
-pub const SUBCOMMAND_DAEMON: &str = "daemon";
-pub const SUBCOMMAND_RULES_UPGRADE: &str = "rules-upgrade";
-
-pub fn arg_matches<'a>() -> ArgMatches<'a> {
-    App::new("tornado_daemon")
-        .arg(Arg::with_name("config-dir")
-            .long("config-dir")
-            .help("The filesystem folder where the Tornado configuration is saved")
-            .default_value(CONFIG_DIR_DEFAULT.unwrap_or("/etc/tornado")))
-        .arg(Arg::with_name("rules-dir")
-            .long("rules-dir")
-            .help("The folder where the processing tree configuration is saved in JSON format. This folder is relative to the `config-dir`")
-            .default_value("/rules.d/"))
-        .arg(Arg::with_name("drafts-dir")
-            .long("drafts-dir")
-            .help("The folder where the configuration drafts are saved in JSON format. This folder is relative to the `config-dir`")
-            .default_value("/drafts/"))
-        .subcommand(structopt::clap::SubCommand::with_name(SUBCOMMAND_CHECK )
-            .help("Checks that the configuration is valid"))
-        .subcommand(structopt::clap::SubCommand::with_name(SUBCOMMAND_DAEMON )
-            .help("Starts the Tornado daemon"))
-        .subcommand(structopt::clap::SubCommand::with_name(SUBCOMMAND_RULES_UPGRADE)
-            .help("Starts the Tornado Rules upgrade process"))
-        .get_matches()
-}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "tornado")]
--- tornado/engine/src/main.rs
+++ tornado/engine/src/main.rs
@@ -1,5 +1,4 @@
-use crate::config::{SUBCOMMAND_CHECK, SUBCOMMAND_DAEMON, SUBCOMMAND_RULES_UPGRADE, Opt, SubCommand};
-use log::error;
+use crate::config::{Opt, SubCommand};
 use structopt::StructOpt;
 
 pub mod actor;
@@ -10,27 +9,7 @@ mod monitoring;
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    /*
-    let arg_matches = config::arg_matches();
 
-    let config_dir = arg_matches.value_of("config-dir").expect("config-dir should be provided");
-    let rules_dir = arg_matches.value_of("rules-dir").expect("rules-dir should be provided");
-    let drafts_dir = arg_matches.value_of("drafts-dir").expect("drafts-dir should be provided");
-
-    let subcommand = arg_matches.subcommand();
-
-    match subcommand {
-        (SUBCOMMAND_CHECK, _) => command::check::check(config_dir, rules_dir, drafts_dir).await,
-        (SUBCOMMAND_DAEMON, _) => command::daemon::daemon(config_dir, rules_dir, drafts_dir).await,
-        (SUBCOMMAND_RULES_UPGRADE, _) => {
-            command::upgrade_rules::upgrade_rules(config_dir, rules_dir, drafts_dir).await
-        }
-        _ => {
-            error!("Unknown subcommand [{}]", subcommand.0);
-            Ok(())
-        }
-    }
-*/
     let opt: Opt = Opt::from_args();
 
     let config_dir = opt.config_dir();
