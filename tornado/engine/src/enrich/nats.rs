47b1dc4475cb06f8b0e53f504ea0f05c97f7f531
Francesco Cina <mr.francesco.cina@gmail.com>
2021-09-29 15:06:36 +0200

    Add key to nats extractor

--- tornado/engine/src/enrich/nats.rs
+++ tornado/engine/src/enrich/nats.rs
@@ -9,9 +9,10 @@ use tornado_common::actors::message::TornadoCommonActorError;
 #[serde(tag = "type")]
 pub enum NatsExtractor {
     /// Uses a regular expression to extract the tenant_id from the subject name
-    TenantIdFromSubject {
+    FromSubject {
         #[serde(with = "serde_regex")]
-        regex: Regex
+        regex: Regex,
+        key: String,
     }
 }
 
@@ -19,16 +20,16 @@ impl NatsExtractor {
 
     pub fn process(&self, subject: &str, mut event: InternalEvent) -> Result<InternalEvent, TornadoCommonActorError> {
         match self {
-            NatsExtractor::TenantIdFromSubject { regex } => {
+            NatsExtractor::FromSubject { regex, key } => {
                 match regex.captures(subject).and_then(|captures| captures.get(1)) {
                     Some(tenant_id_match) => {
                         let tenant_id = tenant_id_match.as_str();
-                        trace!("tenant_id [{}] extracted from Nats subject [{}]", tenant_id, subject);
-                        event.add_to_metadata("tenant_id".to_owned(), Value::Text(tenant_id.to_owned())).map_err(|err| TornadoCommonActorError::GenericError { message: format! {"{}", err} })?;
+                        trace!("key [{}] value [{}] extracted from Nats subject [{}]", key, tenant_id, subject);
+                        event.add_to_metadata(key.to_owned(), Value::Text(tenant_id.to_owned())).map_err(|err| TornadoCommonActorError::GenericError { message: format! {"{}", err} })?;
                         Ok(event)
                     },
                     None => {
-                        debug!("Cannot extract tenant_id from Nats subject [{}]", subject);
+                        debug!("Cannot extract key [{}] from Nats subject [{}]", key, subject);
                         Ok(event)
                     }
                 }
@@ -49,8 +50,9 @@ mod test {
         // Arrange
         let original_event = InternalEvent::new(Default::default());
 
-        let extractor = NatsExtractor::TenantIdFromSubject {
-            regex: Regex::new("(.*)\\.tornado\\.events").unwrap()
+        let extractor = NatsExtractor::FromSubject {
+            regex: Regex::new("(.*)\\.tornado\\.events").unwrap(),
+            key: "tenant_id".to_owned(),
         };
 
         // Act
@@ -66,8 +68,9 @@ mod test {
         // Arrange
         let original_event = InternalEvent::new(Default::default());
 
-        let extractor = NatsExtractor::TenantIdFromSubject {
-            regex: Regex::new("(.*)\\.tornado\\.events").unwrap()
+        let extractor = NatsExtractor::FromSubject {
+            regex: Regex::new("(.*)\\.tornado\\.events").unwrap(),
+            key: "tenant_id".to_owned(),
         };
 
         // Act
