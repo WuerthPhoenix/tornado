7eec7eaf2c4290c1ce853023958a770b910564a0
Damiano Chini <damiano.chini@wuerth-phoenix.com>
2021-10-13 12:35:03 +0200

    Add logic for adding a filter - part 1

--- tornado/engine/src/command/create_filter.rs
+++ tornado/engine/src/command/create_filter.rs
@@ -1,11 +1,239 @@
-use crate::config::FilterCreateOpt;
+use crate::config::{FilterCreateOpt, parse_config_files};
+use tornado_engine_matcher::config::{MatcherConfigReader, MatcherConfig};
+use tornado_engine_matcher::config::filter::Filter;
+use tornado_common::TornadoError;
 
 pub async fn create_filter(
-    _config_dir: &str,
-    _rules_dir: &str,
-    _drafts_dir: &str,
+    config_dir: &str,
+    rules_dir: &str,
+    drafts_dir: &str,
     opts: &FilterCreateOpt
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    println!("Creating Filter with name {}. Filter definition will be read from {}", opts.name, opts.from_filepath);
+    let filter_name = &opts.name;
+    let filter_filepath = &opts.from_filepath;
+    println!("Creating Filter with name {}. Filter definition will be read from {}", filter_name, filter_filepath);
+
+    let filter = read_filter_from_file(filter_filepath);
+
+    let configs = parse_config_files(config_dir, rules_dir, drafts_dir)?;
+    let config_manager = configs.matcher_config;
+    let mut current_config = config_manager.get_config().await?;
+    add_filter(&mut current_config, filter_name, filter)?;
+
+    // let drafts = config_manager.get_drafts()?;
+    // for draft in drafts {
+    //     config_manager.get_draft(draft)
+    // }
+    // let matcher_config_clone = matcher_config.clone();
+
     Ok(())
+}
+
+fn read_filter_from_file(_filepath: &String) -> Filter {
+    unimplemented!()
+}
+
+fn add_filter(
+    matcher_config: &mut MatcherConfig,
+    filter_to_add_name: &str,
+    filter_to_add: Filter,
+) -> Result<(), TornadoError> {
+    match matcher_config {
+        MatcherConfig::Filter { name: _, filter: _, nodes } => {
+            let node_with_same_name = nodes.iter_mut().find(|node| match node {
+                MatcherConfig::Filter { name, .. } => { name == filter_to_add_name }
+                MatcherConfig::Ruleset { name, .. } => { name == filter_to_add_name }
+            } );
+            if let Some(node_with_same_name) = node_with_same_name {
+                match node_with_same_name {
+                    MatcherConfig::Filter { name, filter, nodes: _ } => {
+                        if filter.filter == filter_to_add.filter && filter.active == filter_to_add.active {
+                            return Ok(());
+                        }
+                        *name = node_backup_name(&name);
+                    }
+                    MatcherConfig::Ruleset { name, rules: _ } => {
+                        *name = node_backup_name(&name);
+                    }
+                }
+            }
+            nodes.push(MatcherConfig::Filter {
+                name: filter_to_add_name.to_string(),
+                filter: filter_to_add,
+                nodes: vec![]
+            })
+        }
+        MatcherConfig::Ruleset { name, rules } => {
+            return Err(TornadoError::ConfigurationError { message: format!("Unexpected ruleset at root level. Ruleset name: {}. Rules: {:?}", name, rules) })
+        }
+    }
+    Ok(())
+}
+
+fn node_backup_name (name: &str) -> String {
+    format!("{}_backup", name)
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use crate::command::upgrade_rules::test::prepare_temp_dirs;
+    use tornado_engine_matcher::config::Defaultable;
+    use tornado_engine_matcher::config::rule::Operator;
+    use tornado_common_api::Value;
+
+    #[tokio::test]
+    async fn should_add_filter_if_not_existing() {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let (config_dir, rules_dir, drafts_dir) = prepare_temp_dirs(&tempdir);
+        let configs = parse_config_files(&config_dir, &rules_dir, &drafts_dir).unwrap();
+
+        let mut matcher_config = configs.matcher_config.get_config().await.unwrap();
+        let matcher_config_before = matcher_config.clone();
+        let filter_to_add_name = "new_filter";
+        let filter_to_add = Filter {
+            description: "my new filter".to_string(),
+            active: true,
+            filter: Defaultable::Default {},
+        };
+
+        // Act
+        add_filter(&mut matcher_config, filter_to_add_name, filter_to_add.clone()).unwrap();
+
+        // Assert
+        assert_ne!(matcher_config_before, matcher_config);
+        match matcher_config {
+            MatcherConfig::Filter { name, filter: _, nodes } => {
+                assert_eq!(name, "root");
+                assert_eq!(nodes.len(), 5);
+                match nodes.get(4).unwrap() {
+                    MatcherConfig::Filter { name, filter: resulting_filter, nodes } => {
+                        assert_eq!(name, filter_to_add_name);
+                        assert_eq!(resulting_filter, &filter_to_add);
+                        assert_eq!(nodes, &vec![]);
+                    }
+                    MatcherConfig::Ruleset { .. } =>
+                        {
+                            assert!(false)
+                        }
+                }
+            }
+            MatcherConfig::Ruleset { .. } => assert!(false)
+        }
+    }
+
+    #[tokio::test]
+    async fn should_add_filter_and_backup_the_existing_one_if_different() {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let (config_dir, rules_dir, drafts_dir) = prepare_temp_dirs(&tempdir);
+        let configs = parse_config_files(&config_dir, &rules_dir, &drafts_dir).unwrap();
+
+        let mut matcher_config = configs.matcher_config.get_config().await.unwrap();
+        let matcher_config_before = matcher_config.clone();
+        let filter_to_add_name = "tenant_id_alpha";
+        let filter_to_add = Filter {
+            description: "my new filter".to_string(),
+            active: true,
+            filter: Defaultable::Value(Operator::Equals {
+                first: Value::Text("1".to_owned()),
+                second: Value::Text("1".to_owned()),
+            }),
+        };
+
+        // Act
+        add_filter(&mut matcher_config, filter_to_add_name, filter_to_add.clone()).unwrap();
+
+        // Assert
+        assert_ne!(matcher_config_before, matcher_config);
+        match matcher_config {
+            MatcherConfig::Filter { name, filter: _, nodes } => {
+                assert_eq!(name, "root");
+                assert_eq!(nodes.len(), 5);
+                let backup_node = nodes.iter().find(|node| match node {
+                    MatcherConfig::Filter { name, .. } => { name == "tenant_id_alpha_backup"}
+                    _ => false
+                });
+                assert_eq!(backup_node.unwrap().get_direct_child_nodes_count(), 1);
+
+                let added_node = nodes.iter().find(|node| match node {
+                    MatcherConfig::Filter { name, .. } => { name == filter_to_add_name}
+                    _ => false
+                });
+                assert_eq!(added_node.unwrap().get_direct_child_nodes_count(), 0);
+            }
+            MatcherConfig::Ruleset { .. } => assert!(false)
+        }
+    }
+
+    #[tokio::test]
+    async fn should_add_filter_and_backup_the_existing_ruleset_with_same_name() {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let (config_dir, rules_dir, drafts_dir) = prepare_temp_dirs(&tempdir);
+        let configs = parse_config_files(&config_dir, &rules_dir, &drafts_dir).unwrap();
+
+        let mut matcher_config = configs.matcher_config.get_config().await.unwrap();
+        let matcher_config_before = matcher_config.clone();
+        let filter_to_add_name = "ruleset_01";
+        let filter_to_add = Filter {
+            description: "my new filter".to_string(),
+            active: true,
+            filter: Defaultable::Value(Operator::Equals {
+                first: Value::Text("1".to_owned()),
+                second: Value::Text("1".to_owned()),
+            }),
+        };
+
+        // Act
+        add_filter(&mut matcher_config, filter_to_add_name, filter_to_add.clone()).unwrap();
+
+        // Assert
+        assert_ne!(matcher_config_before, matcher_config);
+        match matcher_config {
+            MatcherConfig::Filter { name, filter: _, nodes } => {
+                assert_eq!(name, "root");
+                assert_eq!(nodes.len(), 5);
+                let backup_node = nodes.iter().find(|node| match node {
+                    MatcherConfig::Filter { name, .. } => { name == "ruleset_01_backup"}
+                    _ => false
+                });
+                assert_eq!(backup_node.unwrap().get_direct_child_nodes_count(), 1);
+
+                let added_node = nodes.iter().find(|node| match node {
+                    MatcherConfig::Filter { name, .. } => { name == filter_to_add_name}
+                    _ => false
+                });
+                assert_eq!(added_node.unwrap().get_direct_child_nodes_count(), 0);
+            }
+            MatcherConfig::Ruleset { .. } => assert!(false)
+        }
+    }
+
+    #[tokio::test]
+    async fn should_not_modify_config_if_filter_exists() {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let (config_dir, rules_dir, drafts_dir) = prepare_temp_dirs(&tempdir);
+        let configs = parse_config_files(&config_dir, &rules_dir, &drafts_dir).unwrap();
+
+        let mut matcher_config = configs.matcher_config.get_config().await.unwrap();
+        let matcher_config_before = matcher_config.clone();
+        let filter_to_add_name = "tenant_id_alpha";
+        let filter_to_add = Filter {
+            description: "my new filter".to_string(),
+            active: true,
+            filter: Defaultable::Value(Operator::Equals {
+                first: Value::Text("${event.metadata.tenant_id}".to_owned()),
+                second: Value::Text("alpha".to_owned()),
+            }),
+        };
+
+        // Act
+        add_filter(&mut matcher_config, filter_to_add_name, filter_to_add.clone()).unwrap();
+
+        // Assert
+        assert_eq!(matcher_config_before, matcher_config);
+    }
 }
