73ba805f589c10ee04e46e2817f14764afb3a384
Francesco Cina <mr.francesco.cina@gmail.com>
2021-10-21 10:38:04 +0200

    Add actix_web metrics endpoint tests

--- common/metrics/src/endpoint/actix_web.rs
+++ common/metrics/src/endpoint/actix_web.rs
@@ -19,8 +19,54 @@ async fn text_metrics(metrics: Data<Metrics>) -> HttpResponse {
         opentelemetry::global::handle_error(MetricsError::Other(err.to_string()));
     }
 
-    //let body = String::from_utf8(buf).unwrap_or_default();
     HttpResponse::Ok()
         .insert_header((actix_web::http::header::CONTENT_TYPE, prometheus::TEXT_FORMAT))
         .body(buf)
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use actix_web::{test, App};
+    use actix_web::http::StatusCode;
+    use opentelemetry::Key;
+
+    #[actix_rt::test]
+    async fn should_expose_a_metrics_endpoint() {
+
+        // Arrange
+        let metrics = Arc::new(Metrics::default());
+        let mut srv = test::init_service(
+            App::new().service(metrics_endpoints(metrics.clone())),
+        )
+            .await;
+
+        // Record a metric
+        {
+            let labels = vec![
+                Key::from_static_str("test").string("something"),
+            ];
+            metrics.http_requests_counter.add(1, &labels);
+            metrics.http_requests_duration_seconds.record(
+                123f64,
+                &labels,
+            );
+        }
+
+        let request =
+            test::TestRequest::get().uri("/metrics/text").to_request();
+
+        // Act
+        let response = test::call_service(&mut srv, request).await;
+
+        // Assert
+        assert_eq!(StatusCode::OK, response.status());
+
+        let metrics = test::read_body(response).await;
+        let content = std::str::from_utf8(&metrics).unwrap();
+        assert!(content.contains(r#"test="something""#))
+    }
+
 }
