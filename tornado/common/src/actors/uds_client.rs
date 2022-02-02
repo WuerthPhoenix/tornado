ebd195b001e80871f81b06b995d3a7d29c693d55
William Calliari <william.calliari@wuerth-phoenix.com>
2022-02-02 15:05:59 +0100

    Remove field `trace_id` from Event

--- tornado/common/src/actors/uds_client.rs
+++ tornado/common/src/actors/uds_client.rs
@@ -74,8 +74,7 @@ impl Handler<EventMessage> for UdsClientActor {
     type Result = Result<(), TornadoCommonActorError>;
 
     fn handle(&mut self, msg: EventMessage, ctx: &mut Context<Self>) -> Self::Result {
-        let trace_id = msg.event.trace_id.as_str();
-        let _span = tracing::error_span!("UdsClientActor", trace_id).entered();
+        let _span = tracing::error_span!("UdsClientActor").entered();
         trace!("UdsClientActor - Handling Event to be sent through UDS - {:?}", &msg.event);
 
         match &mut self.tx {
