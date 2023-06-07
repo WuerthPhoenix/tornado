481c89ea484108453fb77328ce64955c62915613
William Calliari <william.calliari@wuerth-phoenix.com>
2023-06-07 09:58:00 +0200

    Change edit endpoint method and implement test for it.

--- tornado/engine_api/src/config/web.rs
+++ tornado/engine_api/src/config/web.rs
@@ -101,7 +101,7 @@ pub fn build_config_v2_endpoints<
                         "/rule/details/{param_auth}/{draft_id}/{ruleset_path}/{rule_name}",
                     )
                     .route(web::get().to(get_draft_rule_details::<A, CM>))
-                    .route(web::post().to(edit_draft_rule_details::<A, CM>)),
+                    .route(web::put().to(edit_draft_rule_details::<A, CM>)),
                 ),
         )
         .service(
@@ -1590,6 +1590,50 @@ mod test {
             .uri("/config/draft/rule/details/auth1/draft123/root,child_2")
             .set_json(&RuleDto {
                 name: "rule-1".to_string(),
+                description: "nothing relevant".to_string(),
+                do_continue: false,
+                active: true,
+                constraint: ConstraintDto { where_operator: None, with: Default::default() },
+                actions: vec![],
+            })
+            .to_request();
+
+        let response = test::call_service(&mut srv, request).await;
+
+        // Assert
+        assert_eq!(StatusCode::OK, response.status());
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    async fn v2_endpoint_edit_rule_in_draft_by_path_should_return_ok() -> Result<(), ApiError> {
+        // Arrange
+        let mut srv =
+            test::init_service(App::new().service(build_config_v2_endpoints(ApiDataV2 {
+                auth: test_auth_service_v2(),
+                api: ConfigApi::new(TestApiHandler {}, Arc::new(ConfigManager {})),
+            })))
+            .await;
+
+        // Act
+        let request = test::TestRequest::put()
+            .insert_header((
+                header::AUTHORIZATION,
+                AuthServiceV2::auth_to_token_header(&AuthHeaderV2 {
+                    user: "user".to_string(),
+                    auths: auth_map(
+                        "auth1",
+                        Authorization {
+                            path: vec!["root".to_owned()],
+                            roles: vec!["edit".to_owned()],
+                        },
+                    ),
+                    preferences: None,
+                })?,
+            ))
+            .uri("/config/draft/rule/details/auth1/draft123/root,child_2/rule_1")
+            .set_json(&RuleDto {
+                name: "rule_2".to_string(),
                 description: "nothing relevant".to_string(),
                 do_continue: false,
                 active: true,
