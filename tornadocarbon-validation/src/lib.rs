b10f99c5a3520edaac3a90b1ff68b4cc8b536f02
root <root@DESKTOP-R8CITVI.localdomain>
2023-08-09 14:45:24 +0300

    update

--- tornadocarbon-validation/src/lib.rs
+++ tornadocarbon-validation/src/lib.rs
@@ -1,9 +1,11 @@
 use wasm_bindgen::prelude::*;
+use error::Error;
+mod error;
 
 #[wasm_bindgen]
 pub struct ValidationResult {
     is_valid: bool,
-    error: Option<String>,
+    error: Option<Error>,
 }
 
 #[wasm_bindgen]
@@ -14,7 +16,7 @@ impl ValidationResult {
     }
 
     #[wasm_bindgen(getter)]
-    pub fn error(&self) -> Option<String> {
+    pub fn error(&self) -> Option<Error> {
         self.error.clone()
     }
 }
@@ -23,6 +25,8 @@ impl ValidationResult {
 pub fn validate_regex(reg_exp: &str) -> ValidationResult {
     match regex_syntax::parse(reg_exp) {
         Ok(_) => ValidationResult { is_valid: true, error: None },
-        Err(e) => ValidationResult { is_valid: false, error: Some(e.to_string()) },
+        Err(Error::Parse(e)) => ValidationResult { is_valid: false, error: Some(e.into()) },
+        Err(Error::Translate(e)) => ValidationResult { is_valid: false, error: Some(e.into()) },
+        Err(_) => ValidationResult { is_valid: false, error: None },
     }
 }
