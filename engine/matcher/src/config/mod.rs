a3aeab54510686b51aed4b51ff1579e23d577dd8
William Calliari <william.calliari@wuerth-phoenix.com>
2024-02-26 13:53:29 +0100

    Remove last node name for the node creation API endpoint

--- engine/matcher/src/config/mod.rs
+++ engine/matcher/src/config/mod.rs
@@ -161,7 +161,7 @@ impl MatcherConfig {
                 message: "The node path must specify a parent node".to_string(),
             });
         }
-        let path_to_parent = &path[0..path.len() - 1];
+        let path_to_parent = &path[0..path.len()];
         let current_node = self.get_mut_node_by_path_or_err(path_to_parent)?;
 
         if current_node.get_child_node_by_name(node.get_name()).is_some() {
@@ -833,14 +833,11 @@ mod tests {
         };
 
         // Act
-        let result_not_existing =
-            config.create_node_in_path(&["root", "filter3", "new_filter"], &new_filter);
-        let result_ruleset = config.create_node_in_path(
-            &["root", "filter2", "filter3", "ruleset1", "new_filter"],
-            &new_filter,
-        );
+        let result_not_existing = config.create_node_in_path(&["root", "filter3"], &new_filter);
+        let result_ruleset =
+            config.create_node_in_path(&["root", "filter2", "filter3", "ruleset1"], &new_filter);
         let result_already_existing_node =
-            config.create_node_in_path(&["root", "filter1", "new_filter"], &new_filter);
+            config.create_node_in_path(&["root", "filter1"], &new_filter);
 
         // Assert
         assert!(result_not_existing.is_err());
@@ -861,7 +858,9 @@ mod tests {
         assert_eq!(
             result_already_existing_node.err(),
             Some(MatcherError::ConfigurationError {
-                message: "A node with name \"new_filter\" already exists in path [\"root\", \"filter1\", \"new_filter\"]".to_string(),
+                message:
+                    "A node with name \"new_filter\" already exists in path [\"root\", \"filter1\"]"
+                        .to_string(),
             })
         );
     }
@@ -966,8 +965,7 @@ mod tests {
         };
 
         // Act
-        let result =
-            config.create_node_in_path(&["root", "filter2", "filter3", "new_filter"], &new_filter);
+        let result = config.create_node_in_path(&["root", "filter2", "filter3"], &new_filter);
 
         // Assert
         assert!(result.is_ok());
